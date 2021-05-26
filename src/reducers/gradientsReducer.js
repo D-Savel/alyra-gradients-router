bb3f35915088bf375562fae0861336a7560223d2
CryptoThibault <thibault.chalaoux@gmail.com>
2021-05-26 18:13:12 +0200

    context

---
+++ src/reducers/gradientsReducer.js
@@ -0,0 +1,24 @@
+const gradientsReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return {
+        ...state,
+        loading: true,
+      }
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        gradients: action.payload
+      }
+    case "FETCH_FAILURE":
+      return {
+        ...state,
+        error: action.payload,
+      }
+    default:
+      throw new Error(`Unsupported action type ${action.type}`)
+  }
+}
+
+export default gradientsReducer;
